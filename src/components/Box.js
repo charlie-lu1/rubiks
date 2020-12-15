8f5095f0fa49f3be171875f51fe02597fce1962f
zavedev <zavedev1@gmail.com>
2020-12-15 14:26:19 -0500

    added colors for box backgrounds

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,10 +1,6 @@
-import React from 'react'
+import React from "react";
 
-export default function Box({box}) {
-
-    return (
-        <div className="this-is-a-box">
-            {box} box
-        </div>
-    )
+export default function Box({ box }) {
+  const color = box.split(/(\d)/)[0];
+  return <div className={`this-is-a-box ${color}`}>{box} box</div>;
 }
